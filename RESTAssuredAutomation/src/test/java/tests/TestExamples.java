855185f3f3914d6be73266bd395ff503749677e4
ma.victoria.cabrera <mvscabrera@yahoo.com>
2022-04-28 12:05:03 +0800

    First commit

---
+++ RESTAssuredAutomation/src/test/java/tests/TestExamples.java
@@ -0,0 +1,52 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class TestExamples {
+	
+	@Test
+	public void test_1() {
+		
+		Response response = get("https://reqres.in/api/users?page=2");
+		
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+		
+		int statusCode = response.statusCode();
+		
+		Assert.assertEquals(statusCode, 200);
+		
+	}
+	
+	@Test
+	public void test_2() {
+		
+		baseURI = "https://reqres.in/api";
+		given().
+			get("/users?page=2").
+		then().
+			statusCode(200).
+			body("data[1].id", equalTo(8)).
+			body("data[1].first_name", equalTo("Lindsay"));
+	
+	}
+	
+	@Test
+	public void test_3() {
+		given().
+			get("/users/23").
+		then().
+			statusCode(404);
+	
+	}
+	
+}
